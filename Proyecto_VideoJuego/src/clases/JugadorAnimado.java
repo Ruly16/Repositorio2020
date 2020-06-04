1d1ccd32d8338584cc4735d6e65c0a5746851066
Ruly16 <57820010+Ruly16@users.noreply.github.com>
2020-06-03 23:31:58 -0600

    Funez
    Ninguna

---
+++ Proyecto_VideoJuego/src/clases/JugadorAnimado.java
@@ -0,0 +1,151 @@
+package clases;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import implementacion.Juego;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class JugadorAnimado extends ObjetoJuego{
+	private int vidas;
+	private HashMap<String, Animacion> animaciones;
+	private int xImagen;
+	private int yImagen;
+	private int anchoImagen;
+	private int altoImagen;
+	private String animacionActual;
+	private int direccion=1;
+	
+	public int getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(int direccion) {
+		this.direccion = direccion;
+	}
+
+	public JugadorAnimado(int x, int y, String nombreImagen, int velocidad, int vidas, String animacionActual) {
+		super(x, y, nombreImagen, velocidad);
+		this.vidas = vidas;
+		this.animacionActual = animacionActual;
+		animaciones = new HashMap<String, Animacion>();
+		inicializarAnimaciones();
+	}
+	
+	public int getVidas() {
+		return vidas;
+	}
+	public void setVidas(int vidas) {
+		this.vidas = vidas;
+	}
+	
+	public void inicializarAnimaciones() {
+		Rectangle coordenadasCorrer[]= {
+				/*new Rectangle(13, 229, 75,68),
+				new Rectangle(100,229, 75,68),
+				new Rectangle(171,229, 75,68),
+				new Rectangle(230,229, 75,68),
+				new Rectangle(287,224, 75,73),
+				new Rectangle(423,229, 75,68),
+				new Rectangle(500,229, 75,68),
+				new Rectangle(576,229, 75,68),
+				new Rectangle(640,229, 75,68),
+				new Rectangle(699,229, 75,68),
+				new Rectangle(764,229, 75,68),
+				new Rectangle(836,229, 75,73),
+				new Rectangle(907,229, 75,68)  */
+				
+				//Mis Animaciones Correr
+				new Rectangle(3,214,    190,160),
+				new Rectangle(200,214,  190,160),
+				new Rectangle(395,214,  190,160),
+				new Rectangle(590,214,  190,160),
+				new Rectangle(785,214,  190,160),
+				new Rectangle(980,214,  190,160),
+				new Rectangle(1175,214, 190,160),
+				new Rectangle(1370,214, 190,160),
+				new Rectangle(1565,214, 190,160),
+				new Rectangle(1760,214, 190,160)
+		};
+		
+		Animacion animacionCorrer = new Animacion(0.05,coordenadasCorrer);
+		animaciones.put("correr", animacionCorrer);
+		
+		Rectangle coordenadasDescanso[] = {
+				/*new Rectangle(26, 16, 62,72),
+				new Rectangle(89,16, 62,72),
+				new Rectangle(153,16, 62,72),
+				new Rectangle(225,16, 62,72)*/
+				
+				//Mis Animaciones Descanso
+				new Rectangle(45,15,   190,130),
+				new Rectangle(230,15,  190,130),
+				new Rectangle(427,15,  190,130),
+				new Rectangle(620,15,  190,130),
+				new Rectangle(810,15,  190,130),
+				new Rectangle(1008,15, 190,130),
+				new Rectangle(1200,15, 190,130),
+				new Rectangle(1400,15, 190,130),
+				new Rectangle(1584,15, 190,130),
+				new Rectangle(1776,15, 190,130)
+		};
+		
+		Animacion animacionDescanso = new Animacion(0.1, coordenadasDescanso);
+		animaciones.put("descanso",animacionDescanso);
+	}
+	
+	public void calcularFrame(double t) {
+		Rectangle coordenadas = animaciones.get(animacionActual).calcularFrameActual(t);
+		this.xImagen = (int)coordenadas.getX();
+		this.yImagen = (int)coordenadas.getY();
+		this.altoImagen = (int)coordenadas.getWidth();
+		this.anchoImagen = (int)coordenadas.getHeight();		
+	}
+	public Rectangle obtenerRectangulo() {
+		return new Rectangle(x, y, (direccion*anchoImagen) - 10, altoImagen);
+	}
+	//Se ejecuta por cada iteracion del ciclo de juego
+	@Override
+	public void pintar(GraphicsContext graficos) {
+		graficos.drawImage(Juego.imagenes.get(nombreImagen),xImagen,yImagen,anchoImagen,altoImagen, x + (direccion==-1?anchoImagen:0), y, direccion*anchoImagen, altoImagen);//xImage, yImagen, anchoFragmento, altoFragmento, xPintar, yPintar, anchoPintar, altoPintar
+		//graficos.setStroke(Color.RED);
+		//graficos.strokeRect(x, y, anchoImagen - 10, altoImagen);
+	}
+	
+	//Se ejecuta por cada iteracion del ciclo de juego
+	@Override
+	public void mover() {
+		if (x>700)
+			x=-80;
+		
+		if (Juego.derecha)//Mover hacia la derecha
+			x+=velocidad;
+		
+		if (Juego.izquierda)//Mover hacia la derecha
+			x-=velocidad;
+		
+//		if (Juego.arriba)
+//			y-=velocidad;
+//		
+//		if (Juego.abajo)
+//			y+=velocidad;
+	}
+
+	public String getAnimacionActual() {
+		return animacionActual;
+	}
+
+	public void setAnimacionActual(String animacionActual) {
+		this.animacionActual = animacionActual;
+	}
+	
+	public void verificarColisionesItem(Item item) {
+		if (!item.isCapturado() && this.obtenerRectangulo().getBoundsInLocal().intersects(item.obtenerRectangulo().getBoundsInLocal())) {
+			this.vidas += item.getCantidadVidas();
+			item.setCapturado(true);
+		}
+			//System.out.println("Estan colisionando");
+	}
+}
---
+++ Proyecto_VideoJuego/src/implementacion/Juego.java
@@ -0,0 +1,225 @@
+package implementacion;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import clases.Fondo;
+import clases.Item;
+import clases.JugadorAnimado;
+import clases.Tile;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+public class Juego extends Application{
+	private GraphicsContext graficos;
+	private Group root;
+	private Scene escena;
+	private Canvas lienzo;
+	//private Jugador jugador;
+	private JugadorAnimado jugadorAnimado;
+	private Fondo fondo;
+	public static boolean arriba;
+	public static boolean abajo;
+	public static boolean izquierda;
+	public static boolean derecha;
+	public static HashMap<String, Image> imagenes;
+	
+	private Item item;
+	private Item item1;
+	private Item item2;
+		
+	//private Tile tile;
+	private ArrayList<Tile> tiles;
+
+	private int tilemap[][] = {
+			{0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0},
+			{5,5,5,5,5,5,5,5,5,5},
+			{7,7,7,7,7,7,7,7,7,7},
+			{7,7,7,7,7,7,7,7,7,7},
+			{7,7,7,7,7,7,7,7,7,7}
+	};
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage ventana) throws Exception {
+		inicializarComponentes();
+		gestionEventos();
+		ventana.setScene(escena);
+		ventana.setTitle("Proyecto Genial Ruly");
+		ventana.show();
+		cicloJuego();
+	}
+	
+	public void cicloJuego() {
+		long tiempoInicial = System.nanoTime();
+		AnimationTimer animationTimer = new AnimationTimer() {
+
+			//Este metodo se ejecuta aproximadamente 60 veces por segundo 60FPS
+			@Override
+			public void handle(long tiempoActual) {
+				double t = (tiempoActual - tiempoInicial) / 1000000000.0;
+				//System.out.println(t);
+				actualizarEstado(t);
+				pintar();
+			}
+			
+		};
+		
+		animationTimer.start();//Empieza el ciclo de juego
+	}
+	
+	public void actualizarEstado(double t) {
+		//jugador.mover();
+		jugadorAnimado.verificarColisionesItem(item);
+		jugadorAnimado.verificarColisionesItem(item1);
+		jugadorAnimado.verificarColisionesItem(item2);
+		jugadorAnimado.calcularFrame(t);
+		jugadorAnimado.mover();
+		fondo.mover();
+	}
+	
+	public void inicializarComponentes() {
+		imagenes = new HashMap<String,Image>();
+		cargarImagenes();
+		//jugador = new Jugador(20, 40, "goku", 3, 0);
+		jugadorAnimado = new JugadorAnimado(20, 250, "ninja", 3, 0,"descanso");
+		fondo = new Fondo(0,0,"fondo1","fondo1",7);
+		inicializarTiles();
+		item = new Item(200,355, "item1",0, 1);
+		item1 = new Item(250,355, "item2",0, 1);
+		item2 = new Item(300,355, "item",0, 1);
+		
+		//tile = new Tile(0,0,"tilemap", 0, 420, 490, 70, 70);
+		root = new Group();
+		escena = new Scene(root, 700, 500);
+		lienzo = new Canvas(700, 500);
+		root.getChildren().add(lienzo);
+		graficos = lienzo.getGraphicsContext2D();
+	}
+	
+	public void inicializarTiles() {
+		tiles = new ArrayList<Tile>();
+		for(int i=0;i<tilemap.length;i++) {
+			for(int j=0;j<tilemap[i].length;j++) {
+				if (tilemap[i][j]!=0)
+					this.tiles.add(new Tile(tilemap[i][j],j*70,i*70,"tilemap",0,70,70));
+				
+			}
+			
+		}
+		
+	}
+	public void cargarImagenes() {
+		imagenes.put("goku", new Image("goku.png"));
+		imagenes.put("goku-furioso", new Image("goku-furioso.png"));
+		imagenes.put("background",new Image("background.jpg"));
+		imagenes.put("background2",new Image("background2.jpg"));
+		imagenes.put("tilemap",new Image("tilemap.png"));
+		imagenes.put("megaman", new Image("megaman.png"));
+		imagenes.put("item", new Image("item.png"));
+		//Mis Imagenes 
+		imagenes.put("fondo1",new Image("fondo1.jpg"));
+		imagenes.put("fondo2",new Image("fondo2.jpg"));
+		imagenes.put("ninja",new Image("ninja.png"));
+		imagenes.put("item1", new Image("itemspuntos.png"));
+		imagenes.put("item2", new Image("itemsvida.png"));
+	}
+	
+	public void pintar() {
+		//graficos.fillRect(0,0,100,100);
+		fondo.pintar(graficos); 
+		//tile.pintar(graficos);
+		for(int i=0;i<tiles.size();i++)
+			tiles.get(i).pintar(graficos);
+		//jugador.pintar(graficos);
+		jugadorAnimado.pintar(graficos);
+		item.pintar(graficos);
+		item1.pintar(graficos);
+		item2.pintar(graficos);
+		graficos.fillText("Vidas: " + jugadorAnimado.getVidas(), 20, 20);
+	}
+	
+	public void gestionEventos() {
+		//escena.setOnKeyPressed(new Evento());
+		escena.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			
+			//El metodo handle se ejecuta cada vez que presiono una tecla.
+			@Override
+			public void handle(KeyEvent evento) {
+				switch(evento.getCode().toString()) {
+					case "RIGHT":
+						derecha = true;
+						jugadorAnimado.setDireccion(1);
+						jugadorAnimado.setAnimacionActual("correr");
+						break;
+					case "LEFT":
+						izquierda = true;
+						jugadorAnimado.setDireccion(-1);
+						jugadorAnimado.setAnimacionActual("correr");
+						break;
+					case "UP":
+						arriba = true;
+						break;
+					case "DOWN":
+						abajo = true;
+						break;
+					case "SPACE":
+						//jugador.setVelocidad(15);
+						//jugador.setNombreImagen("goku-furioso");
+						jugadorAnimado.setVelocidad(10);
+						
+						break;
+				}
+			}
+		});
+		
+		escena.setOnKeyReleased(new EventHandler<KeyEvent>() {
+
+			//Este metodo se ejecuta cuando se suelta una tecla
+			@Override
+				public void handle(KeyEvent evento) {
+					switch(evento.getCode().toString()) {
+					case "RIGHT":
+						derecha = false;
+						jugadorAnimado.setAnimacionActual("descanso");
+						break;
+					case "LEFT":
+						izquierda = false;
+						jugadorAnimado.setAnimacionActual("descanso");
+						break;
+					case "UP":
+						arriba = false;
+						break;
+					case "DOWN":
+						abajo = false;
+						break;
+					case "SPACE":
+						//jugador.setVelocidad(5);
+						//jugador.setNombreImagen("goku");
+						jugadorAnimado.setVelocidad(5);
+						break;
+				}
+			}
+			
+		});
+	}
+	
+	
+
+}
+
